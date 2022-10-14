8a8e5cd0867dece4eb682df94d1f0c91bd113215
SR-Rubel <rubel162765@gmail.com>
2022-10-14 12:24:43 +0600

    qr code scanner and reader added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import QrCode from './QrCode';
+import QrCodeOutput from './QrCodeOutput';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{height:"500px",width:"500px",margin:"10rem 0 0 10rem"}}>
+        <QrCode />
+        <QrCodeOutput />
+      </div>
     </div>
   );
 }
---
+++ src/QrCode.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import QRCode from "react-qr-code";
+import AES from "crypto-js/aes";
+
+export default function QrCode() {
+  let value = "Hey! this worked \n";
+  const ciphertext = AES.encrypt("this my id 123", "secret key 123").toString();
+  value = value+ciphertext;
+  console.log(value + ciphertext);
+  return (
+    <div>
+      <QRCode
+        size={256}
+        style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+        value={value}
+        viewBox={`0 0 256 256`}
+      />
+    </div>
+  );
+}
