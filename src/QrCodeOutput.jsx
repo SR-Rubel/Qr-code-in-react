6b859726a31af2400b9183dd0118b242e4db82da
SR-Rubel <rubel162765@gmail.com>
2022-10-14 12:47:02 +0600

    utf-8 changed

--- src/QrCodeOutput.jsx
+++ src/QrCodeOutput.jsx
@@ -1,6 +1,7 @@
 import React,{useState} from "react";
 import { QrReader } from "react-qr-reader";
 import AES from "crypto-js/aes";
+import CryptoJS from 'crypto-js'
 function QrCodeOutput() {
   const [data, setData] = useState("No result");
   return (
@@ -11,7 +12,7 @@ function QrCodeOutput() {
             if (!!result) {
               const text = result?.text?.split("\n")
               const plaintext =AES.decrypt(text[1], 'secret key 123');
-              setData(text[0]+'\n'+plaintext);
+              setData(text[0]+'\n'+plaintext.toString(CryptoJS.enc.Utf8));
             }
             if (!!error) {
               console.info(error);
